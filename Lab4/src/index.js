230f60ec2ec00a51c81fc37d1fd084197c609736
Darkhan <darkhan4013@gmail.com>
2020-04-28 14:06:01 +0300

    fix

--- Lab4/src/index.js
+++
@@ -1,6 +0,0 @@
-import {getAboutMe} from './persInfo';
-import {getPokemon} from './pokemon';
-
-document.getElementById('maintab').onclick = getAboutMe;
-document.getElementById("pokemontab").onclick = getPokemon;
-getAboutMe();
